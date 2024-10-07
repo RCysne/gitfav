6abcfb0291a1a0590214369e14e9fb36528d2c74
Ronaldo Cysne <ronaldocysne@gmail.com>
2024-10-07 15:10:05 -0300

    Atualizando

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 import { FavoritesView } from './js/Favorites.js'
 
 
-
 new FavoritesView('#app')
 
-// PAPAI LINDÃO E FOFO E TANBEN BARRIGUDU
-// LENBRANSA  PRA  SENPRE  ASSINADU  LUISA  COM 6 ANOS
